8fcc562e9d6768b6df3a5eb40205355d73891176
Jason Leon <920402030@qq.com>
2026-05-26 21:44:47 +1000

    chore: merge task migration heads

---
+++ backend/task/migrations/0015_merge_task_0014_heads.py
@@ -0,0 +1,13 @@
+# Generated to merge task migration heads after merging prod-preview.
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("task", "0014_merge_invite_pending_and_taskpin"),
+        ("task", "0014_merge_task_labels_and_pins"),
+    ]
+
+    operations = []
